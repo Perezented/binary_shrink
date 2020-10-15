9a856f78eb0fea26bdd0b7b9345378dad2fffc01
Michael Perez <mperez6473@gmail.com>
2020-10-14 23:58:05 -0500

    Have a dictionary that is keeping track of what 3 characters have been thru. Add to a new string.

--- binary-shrink.py
+++ binary-shrink.py
@@ -1,12 +1,30 @@
-binary1 = '101010100101'
+import string
+asciiLetters = (string.ascii_letters)
 
 
-def convertTriSectionToSingle(str):
-    print(len(str))
+binary1 = '101010100101101010100101101010100101101010100101101010100101101010100101'
+
+
+def convertTriSectionToSingle(binaryString):
+    shortenedBinaryDict = {}
+    print('here is the dict: ', shortenedBinaryDict)
+    shortenedStr = ''
     i = 0
-    while i is not len(str):
-        print(str[i: i + 3])
+    j = 0
+    while i is not len(binaryString):
+        print(shortenedBinaryDict)
+        nextThree = (binaryString[i:i + 3])
+        if nextThree not in shortenedBinaryDict:
+            shortenedBinaryDict[nextThree] = asciiLetters[j]
+            j += 1
+        shortenedStr += shortenedBinaryDict[nextThree]
         i += 3
+
+    print('original binary string: ', binaryString)
+    print('shortenedStr: ', shortenedStr)
+    print(f'Length of original string: {len(binaryString)}')
+    print(f'Length of shortened string: {len(shortenedStr)}')
+    print(f'A {int(len(binaryString)/len(shortenedStr) *100)}% impovement!')
 
 
 convertTriSectionToSingle(binary1)
